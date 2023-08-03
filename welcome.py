64a4cd13aa70de5173bf59b4931f15bc6d659cc0
ExpPetPhil <141242050+ExpPetPhil@users.noreply.github.com>
2023-08-03 10:32:38 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -38,6 +38,8 @@ st.bokeh_chart(p, use_container_width=True)
 
 el_list= df.columns.tolist()[27:80]
 x_axis = st.selectbox('select elements', el_list)
+y_axis = st.selectbox('select elements', el_list)
+
 
 st.multiselect('select location', file_name_list, file_name_list[0])
 
