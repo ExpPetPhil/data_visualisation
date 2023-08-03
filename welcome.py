8844b8e65021e02b0af74260f469fb6df94749d0
ExpPetPhil <141242050+ExpPetPhil@users.noreply.github.com>
2023-08-03 10:45:30 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -17,19 +17,20 @@ for i in os.listdir():
 
 st.write(file_name_list)
 
-df = pd.read_csv('Bastar Craton.csv')
-st.dataframe(df)
+#df = pd.read_csv('Bastar Craton.csv')
+#st.dataframe(df)
 
 
 el_list= df.columns.tolist()[27:80]
 x_axis = st.selectbox('select elements_x', el_list)
 y_axis = st.selectbox('select elements_y', el_list)
 
-st.multiselect('select location', file_name_list, file_name_list[0])
+location = st.selectbox('select location', file_name_list, file_name_list[0])
 
 x = df['Mg']
 y = df['Si']
 
+df = pd.read_csv(location)
 p = figure(
     title='simple line example',
     x_axis_label='x',
