13393069b01901737508fc55af31ad639f15018f
ExpPetPhil <141242050+ExpPetPhil@users.noreply.github.com>
2023-08-03 10:36:53 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -21,6 +21,23 @@ df = pd.read_csv('Bastar Craton.csv')
 st.dataframe(df)
 
 
+
+
+x = df['Mg']
+y = df['Si']
+
+p = figure(
+    title='simple line example',
+    x_axis_label='x',
+    y_axis_label='y')
+
+p.circle(x, y, legend_label='Trend', line_width=2)
+
+st.bokeh_chart(p, use_container_width=True)
+
+
+
+
 el_list= df.columns.tolist()[27:80]
 x_axis = st.selectbox('select elements_x', el_list)
 #y_axis = st.selectbox('select elements_y', el_list)
