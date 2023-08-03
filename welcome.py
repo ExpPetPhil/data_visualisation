2c916615d315671c35e2545845ca92c49f7097c6
ExpPetPhil <141242050+ExpPetPhil@users.noreply.github.com>
2023-08-03 12:53:05 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -37,8 +37,8 @@ y_axis = st.selectbox('select elements_y', el_list)
 
 #x = df['Mg']/10000
 #y = df['Si']/10000
-x = df[x_axis]
-y = df[x_axis]
+x = x_axis
+y = y_axis
 
 p = figure(
     title='simple line example',
