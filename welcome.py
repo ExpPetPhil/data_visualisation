774fa5cb0594aa0b0e0cbbfe045e18226a2531ed
ExpPetPhil <141242050+ExpPetPhil@users.noreply.github.com>
2023-08-03 09:38:14 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -8,5 +8,5 @@ st.write('Hello World')
 df = pd.read_csv('Bastar Craton.csv')
 st.dataframe(df)
 
-el_list= df.colums.tolist()[27:80]
+el_list= df.columns.tolist()[27:80]
 st.selectbox('select elements', el_list)
