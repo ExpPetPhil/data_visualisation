1a2b176560aa36e9369fcfeb93253b726ff8e4e3
ExpPetPhil <141242050+ExpPetPhil@users.noreply.github.com>
2023-08-03 10:25:03 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -4,6 +4,8 @@ import numpy as np
 
 import pandas as pd
 
+from bokeh.plotting import figure
+
 import os
 
 st.write('Hello World')
@@ -18,10 +20,29 @@ st.write(file_name_list)
 df = pd.read_csv('Bastar Craton.csv')
 st.dataframe(df)
 
+
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
+
+p = figure(
+    title='simple line example',
+    x_axis_label='x',
+    y_axis_label='y')
+
+p.line(x, y, legend_label='Trend', line_width=2)
+
+st.bokeh_chart(p, use_container_width=True)
+
+
+
+
 el_list= df.columns.tolist()[27:80]
 x_axis = st.selectbox('select elements', el_list)
 
 st.multiselect('select location', file_name_list, file_name_list[0])
+
+
+
 
 
 
